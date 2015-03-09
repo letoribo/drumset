e8d670a5e9dd9adf82868f761b88399c9239eeee
letoribo <lawegard@yandex.ru>
2015-03-09 22:59:43 +0200

    Collaborative Drums App

---
+++ app.js
@@ -0,0 +1,27 @@
+var express = require('express'),
+app = express(),
+server = require('http').createServer(app),
+io = require('socket.io').listen(server);
+
+app.use(express.static(__dirname + '/public'));
+
+io.sockets.on('connection', function(socket) {
+  socket.on('createNote', function(data) {
+    socket.broadcast.emit('onNoteCreated', data);
+  });
+
+  socket.on('changePattern', function(data) {
+	 socket.broadcast.emit('onPatternChanged', data);
+  });
+
+  socket.on('changeBeat', function(data){
+	 socket.broadcast.emit('onBeatChanged', data);
+  });
+
+  socket.on('deleteNote', function(data){
+	 socket.broadcast.emit('onNoteDeleted', data);
+  });
+});
+
+var port = Number(process.env.PORT || 2311);
+server.listen(port);
